c9233ff50d2b793f76bede74b840fc6d5cfdf8c7
bastien <bastien.allegre@etu.univ-grenoble-alpes.fr>
2025-02-03 18:23:36 +0100

    feat : add work ( project )

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skill from "./components/Skill";
+import Work from "./components/Work";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Hero />
         <About />
         <Skill />
+        <Work />
       </main>
     </>
   );
