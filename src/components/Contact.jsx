be8095953beea6c2e5ca8491124c10096eb5057d
bastien <bastien.allegre@etu.univ-grenoble-alpes.fr>
2025-02-25 18:32:40 +0100

    feat : add gsap scoll animation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -80,11 +80,11 @@ const Contact = () => {
     <section id="contact" className="section">
       <div className="container lg:grid lg:grid-cols-2 lg:items-stretch">
         <div className="mb-12 lg:mb-0 lg:flex lg:flex-col">
-          <h2 className="headline-2 lg:max-w-[12ch]">
+          <h2 className="headline-2 lg:max-w-[12ch] reveal-up">
             Contact me for collaboration
           </h2>
 
-          <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch] lg:max-w-[30ch]">
+          <p className="text-zinc-400 mt-3 mb-8 max-w-[50ch] lg:max-w-[30ch] reveal-up">
             Reach out today to discuss your project needs and start
             collaborating on something amazing!
           </p>
@@ -95,7 +95,7 @@ const Contact = () => {
                 ket={key}
                 href={href}
                 targer="_blank"
-                className="w-12 h-12 grid place-items-center ring-inset ring-2 ring-zinc-50/5 rounded-lg transition-[background-color,color] hover:bg-zinc-50 hover:text-zinc-950 active:bg-zinc-50/80"
+                className="w-12 h-12 grid place-items-center ring-inset ring-2 ring-zinc-50/5 rounded-lg transition-[background-color,color] hover:bg-zinc-50 hover:text-zinc-950 active:bg-zinc-50/80 reveal-up"
               >
                 {icon}
               </a>
@@ -110,7 +110,7 @@ const Contact = () => {
         >
           <div className="mb:grid mb:items-center mb:grid-cols-2 mb:gap-2">
             <div className="mb-4">
-              <label htmlFor="name" className="label">
+              <label htmlFor="name" className="label reveal-up">
                 Name
               </label>
 
@@ -121,12 +121,12 @@ const Contact = () => {
                 autoComplete="name"
                 required
                 placeholder="Bastien Allegre"
-                className="text-field"
+                className="text-field reveal-up"
               />
             </div>
 
             <div className="mb-4">
-              <label htmlFor="email" className="label">
+              <label htmlFor="email" className="label reveal-up">
                 Email
               </label>
 
@@ -137,13 +137,13 @@ const Contact = () => {
                 autoComplete="email"
                 required
                 placeholder="bastien_allegre@orange.fr"
-                className="text-field"
+                className="text-field reveal-up"
               />
             </div>
           </div>
 
           <div className="mb-4">
-            <label htmlFor="message" className="label">
+            <label htmlFor="message" className="label reveal-up">
               Message
             </label>
 
@@ -152,13 +152,13 @@ const Contact = () => {
               id="message"
               placeholder="Salut !"
               required
-              className="text-field resize-y min-h-32 max-h-80"
+              className="text-field resize-y min-h-32 max-h-80 reveal-up"
             ></textarea>
           </div>
 
           <button
             type="submit"
-            className="btn btn-primary [&]:max-w-full w-full justify-center"
+            className="btn btn-primary [&]:max-w-full w-full justify-center reveal-up"
           >
             Envoyer
           </button>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -49,7 +49,7 @@ const Footer = () => {
       <div className="container">
         <div className="lg:grid lg:grid-cols-2">
           <div className="mb-10">
-            <h2 className="headline-1 mb-8 lg:max-w-[12ch]">
+            <h2 className="headline-1 mb-8 lg:max-w-[12ch] reveal-up">
               Let&apos;s work together today!
             </h2>
 
@@ -57,18 +57,19 @@ const Footer = () => {
               href="mailto:bastien_allegre@orange.fr"
               label="Start project"
               icon="chevron_right"
+              classes="reveal-up"
             />
           </div>
 
           <div className="grid grid-cols-2 gap-4 lg:pl-20">
             <div>
-              <p className="mb-2">Sitemap</p>
+              <p className="mb-2 reveal-up">Sitemap</p>
               <ul>
                 {sitemap.map(({ label, href }, key) => (
                   <li key={key}>
                     <a
                       href={href}
-                      className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200"
+                      className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200 reveal-up"
                     >
                       {label}
                     </a>
@@ -78,14 +79,14 @@ const Footer = () => {
             </div>
 
             <div>
-              <p className="mb-2">Socials</p>
+              <p className="mb-2 reveal-up">Socials</p>
               <ul>
                 {socials.map(({ label, href }, key) => (
                   <li key={key}>
                     <a
                       href={href}
                       target="_blank"
-                      className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200"
+                      className="block text-sm text-zinc-400 py-1 transition-colors hover:text-zinc-200 reveal-up"
                     >
                       {label}
                     </a>
@@ -97,11 +98,11 @@ const Footer = () => {
         </div>
 
         <div className="flex items-center justify-between pt-10 mb-8">
-          <a href="" className="">
+          <a href="/" className="logo reveal-up">
             <img src="/images/logo.svg" width={40} height={40} alt="Logo" />
           </a>
 
-          <p className="text-zinc-500 text-sm">
+          <p className="text-zinc-500 text-sm reveal-up">
             &copy; {new Date().getFullYear()}
           </p>
         </div>
