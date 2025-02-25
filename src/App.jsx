a895c535a53402f399f8a7271db7749fc4c37669
bastien <bastien.allegre@etu.univ-grenoble-alpes.fr>
2025-02-25 11:59:58 +0100

    feat : add footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from "./components/About";
 import Skill from "./components/Skill";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
         <Work />
         <Contact />
       </main>
+      <Footer />
     </>
   );
 };
