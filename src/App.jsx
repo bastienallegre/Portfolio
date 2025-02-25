f24eab8d9bcd52f6c72263662ae545a03835638c
bastien <bastien.allegre@etu.univ-grenoble-alpes.fr>
2025-02-25 11:25:48 +0100

    feat: implementation du contact ( problème avec getform )

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skill from "./components/Skill";
 import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <About />
         <Skill />
         <Work />
+        <Contact />
       </main>
     </>
   );
